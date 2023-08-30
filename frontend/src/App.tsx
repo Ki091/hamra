c706195ec489efae1cc6435502c9c4c9c04d36ff
Ki091 <abulifa@outlook.com>
2023-08-30 20:59:11 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>Hamra</header>
+        <main></main>
+        <footer>All Rigths reserved 2023</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
